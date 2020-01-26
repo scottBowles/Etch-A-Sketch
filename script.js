85dc68d926cb54d03982eafbbd825b8429d461ed
Scott Bowles <shbowles@gmail.com>
2020-01-26 00:03:22 -0500

    Add reset button with palette resolution prompt

--- script.js
+++ script.js
@@ -1,38 +1,73 @@
-function createDivs(numberOfDivs) {
-  let arrayOfDivs = []
-  for (i=0; i<numberOfDivs; i++) {
-    let newDiv = document.createElement('div');
-    arrayOfDivs[i] = newDiv;
-  }
-  return arrayOfDivs
-}
-
-let squareDimension = 16;
-let numberOfDivs = squareDimension**2;
-let arrayOfDivs = createDivs(numberOfDivs);
-
+/* These first three lines are here in case we need to change the 
+container size is the future */
 const root = document.querySelector(':root');
 const boxDimensionPixels = 575;
 root.style.setProperty('--box-dimension', boxDimensionPixels + 'px');
 
-const gridTemplateText = "repeat(" + squareDimension + ", 1fr)"
 
-const container = document.querySelector('#container');
-container.style.gridTemplateColumns = gridTemplateText;
-container.style.gridTemplateRows = gridTemplateText;
 
-for (const i in arrayOfDivs) {
-  arrayOfDivs[i].classList.add('tile');
+
+function createGrid(squareDimension) {
+  const container = document.querySelector('#container');
+  const gridTemplateText = "repeat(" + squareDimension + ", 1fr)";
+  container.style.gridTemplateColumns = gridTemplateText;
+  container.style.gridTemplateRows = gridTemplateText;
 }
 
-for (const i in arrayOfDivs) {
-  container.appendChild(arrayOfDivs[i]);
+function createTileDivs(squareDimension) {
+  let numberOfTiles = squareDimension**2;
+  let arrayOfTiles = [];
+  for (i=0; i<numberOfTiles; i++) {
+    let tileDiv = document.createElement('div');
+    arrayOfTiles[i] = tileDiv;
+  }
+  return arrayOfTiles;
 }
 
-arrayOfDivs.forEach(element => {
-  element.addEventListener('mouseover', () => {
-    element.style.backgroundColor = 'grey';
-  })
-});
+function putTilesInGrid(arrayOfTiles) {
+  for (const i in arrayOfTiles) {
+    container.appendChild(arrayOfTiles[i]);
+  }
+}
+
+function addTileMouseoverListeners(arrayOfTiles) {
+  arrayOfTiles.forEach(element => {
+    element.addEventListener('mouseover', () => {
+      element.style.backgroundColor = 'grey';
+    })
+  });
+}
+
+function constructPalette(squareDimension){
+  createGrid(squareDimension);
+  let arrayOfTiles = createTileDivs(squareDimension);
+  putTilesInGrid(arrayOfTiles);
+  addTileMouseoverListeners(arrayOfTiles);
+  return arrayOfTiles;
+}
+
+let squareDimension = 16;
+let arrayOfTiles = constructPalette(squareDimension);
 
 
+
+const resetButton = document.querySelector('#reset');
+
+resetButton.addEventListener('click', resetClick);
+  
+function resetClick() {
+  clearTiles();  
+  let newDimension = +prompt("Reset? Enter new resolution up to 64. For instance, '64' will result in a 64x64 square palette.");
+  if (Number.isInteger(newDimension) && (0 < newDimension < 65)) {
+    arrayOfTiles = constructPalette(newDimension);
+  }else{
+    resetClick();
+  }
+  return arrayOfTiles;
+}
+
+function clearTiles(){
+  for (i in arrayOfTiles) {
+    container.removeChild(arrayOfTiles[i]);
+  }
+}
